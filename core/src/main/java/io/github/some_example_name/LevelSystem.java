08babda4bc0576924dd98e037a019b8eded3effe
SilentPixel <erik.kachovicius@gmail.com>
2026-01-29 12:29:46 +0000

    array can be read from a txt file, no memory of where each level is lcoated when the game is run. level can be changed manually with different value but doesnt change when the player moves up to a direction.

--- core/src/main/java/io/github/some_example_name/LevelSystem.java
+++ core/src/main/java/io/github/some_example_name/LevelSystem.java
@@ -2,6 +2,7 @@ package io.github.some_example_name;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -10,6 +11,8 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.security.Key;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LevelSystem extends ApplicationAdapter {
@@ -19,25 +22,7 @@ public class LevelSystem extends ApplicationAdapter {
     Texture tileset;
     TextureRegion[][] tiles;
     int[][] CurrentLevel;
-    /*int[][] BaseLevel = new int[][]{
-        {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5},
-        {20, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 11, 105},
-        {20, 46, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 51, 105},
-        {20, 46, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 51, 105},
-        {20, 46, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 51, 105},
-        {20, 46, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 51, 105},
-        {20, 46, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 51, 105},
-        {20, 46, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 51, 105},
-        {20, 46, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 51, 105},
-        {20, 46, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 51, 105},
-        {20, 46, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 51, 105},
-        {20, 46, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 51, 105},
-        {20, 46, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 51, 105},
-        {20, 46, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 51, 105},
-        {20, 46, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 51, 105},
-        {20, 106, 107, 107, 107, 107, 107, 107, 107, 107, 107, 107, 107, 107, 107, 107, 107, 107, 107, 107, 107, 107, 107, 107, 107, 107, 107, 107, 111, 105},
-        {120, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 121, 125}
-    }; */
+    ArrayList<int[][]> AllLevels;
 
     @Override
     public void create() {
@@ -46,14 +31,58 @@ public class LevelSystem extends ApplicationAdapter {
         camera.setToOrtho(false, 1920, 1080);
         tileset = new Texture(Gdx.files.internal("assets/AssetsForMaps.png"));
         tiles = TextureRegion.split(tileset, 32, 32);
-        CurrentLevel = new int[31][18];
-        try {
-            CurrentLevel = ReadLevel2DArray();
-            System.out.println("Read Array");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        AllLevels = ReadInLevelsFromFile("assets/Level2DArrays.txt");
+        if (AllLevels.size() > 0){
+            CurrentLevel = AllLevels.get(0);
         }
 
+    }
+
+    public ArrayList<int[][]> ReadInLevelsFromFile(String FileName){
+        ArrayList<int[][]> Levels = new ArrayList<>();
+        try {
+            Scanner sc = new Scanner(Gdx.files.internal(FileName).read());
+            while (sc.hasNextInt()){
+                ArrayList<int[]> rows = new ArrayList<>();
+                while (sc.hasNextInt()){
+                    String line = sc.nextLine().trim();
+                    if (line.isEmpty()){
+                        break;
+                    }
+
+                    String[] tokens = line.split("\\s+");
+                    int[] row = new int[tokens.length];
+                    for (int i = 0; i < tokens.length; i++){
+                        row[i] = Integer.parseInt(tokens[i]);
+                    }
+                    rows.add(row);
+                }
+                if (rows.size() > 0){
+                    int[][] level = new int[rows.size()][];
+                    for (int i =0; i < rows.size(); i++){
+                        level[i] = rows.get(i);
+                    }
+                    Levels.add(level);
+                }
+            }
+            sc.close();
+            System.out.println("Loaded " + Levels.size() + " levels from file");
+        }
+        catch (Exception e){
+            System.err.println("Error loading levels from file: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return Levels;
+    }
+
+    public void SetLevel(int LevelIndex){
+        if (LevelIndex >= 0 && LevelIndex < AllLevels.size()){
+            CurrentLevel = AllLevels.get(LevelIndex);
+            System.out.println("Switched to level " + LevelIndex);
+        }
+        else {
+            System.err.println("Invalid Level index: " + LevelIndex);
+        }
     }
 
     @Override
@@ -65,12 +94,14 @@ public class LevelSystem extends ApplicationAdapter {
         batch.setProjectionMatrix(camera.combined);
 
         batch.begin();
-        for (int i = 0; i < CurrentLevel.length; i++){
-            for (int j = 0; j < CurrentLevel[i].length; j++){
-                int tileX = CurrentLevel[i][j] % tiles[0].length; // column in the tileset
-                int tileY = CurrentLevel[i][j] / tiles[0].length; // row in the tileset
+        if (CurrentLevel != null) {
+            for (int i = 0; i < CurrentLevel.length; i++) {
+                for (int j = 0; j < CurrentLevel[i].length; j++) {
+                    int tileX = CurrentLevel[i][j] % tiles[0].length; // column in the tileset
+                    int tileY = CurrentLevel[i][j] / tiles[0].length; // row in the tileset
 
-                batch.draw(tiles[tileY][tileX], j * 64, (CurrentLevel.length - 1 - i) * 64, 64, 64);
+                    batch.draw(tiles[tileY][tileX], j * 64, (CurrentLevel.length - 1 - i) * 64, 64, 64);
+                }
             }
         }
         batch.end();
@@ -78,20 +109,6 @@ public class LevelSystem extends ApplicationAdapter {
 
     public int[][] getCurrentLevel(){
         return CurrentLevel;
-    }
-
-    public static int[][] ReadLevel2DArray() throws FileNotFoundException{
-        int[][] matrix = new int[31][18];
-        Scanner sc = new Scanner(new File("assets/Level2DArray.txt"));
-
-        for (int i = 0; i < 31; i++){
-            for (int j = 0; j < 18; j++){
-                if (sc.hasNextInt()){
-                    matrix[i][j] = sc.nextInt();
-                }
-            }
-        }
-        return matrix;
     }
 
     @Override
